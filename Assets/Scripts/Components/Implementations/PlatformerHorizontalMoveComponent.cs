83c557130d89042297f2e4d99647a4b1b9ea514e
caothienbao <baocaothien123@gmail.com>
2025-01-18 09:13:34 +0700

    Update movement logic

--- Assets/Scripts/Components/Implementations/PlatformerHorizontalMoveComponent.cs
+++ Assets/Scripts/Components/Implementations/PlatformerHorizontalMoveComponent.cs
@@ -6,9 +6,15 @@ public class PlatformerHorizontalMoveComponent : MonoBehaviour, IMoveable
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
 
+    private PlayerController playerController;
+    public bool IsMoving = false;
+
     private void Start()
     {
         rb = ComponentCache.GetComponent<Rigidbody2D>(gameObject);
+        playerController = ComponentCache.GetComponent<PlayerController>(gameObject);
+
+        IsMoving = playerController.InputReader.IsMoving;
     }
 
     public void Move(Vector3 direction)
